0d2874c35811fa13a858bbac38a532b4198ff043
Gtp87 <fabio.giannitrapani@gmail.com>
2021-11-19 14:19:45 +0100

    add prompt eta and prompt chilometri

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 // Il programma dovrà chiedere all’utente il numero di chilometri che vuole percorrere e l’età del passeggero.
+const eta = parseInt (prompt('indica la tua età'));
+const chilometri = parseInt(prompt('Indica quanti km vuoi percorrere'));
+console.log('Età passeggero: ' + eta);
+console.log('Km passeggero: ' + chilometri);
+
 // Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
 // il prezzo del biglietto è definito in base ai km(0.21 € al km)
 // va applicato uno sconto del 20 % per i minorenni
